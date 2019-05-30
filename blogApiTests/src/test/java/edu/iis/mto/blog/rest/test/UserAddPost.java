5aae65f40ef74b505195c854d310a43f55641a82
Marax97 <44068049+Marax97@users.noreply.github.com>
2019-05-31 00:13:45 +0200

    Only confirmed user can add post

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/UserAddPost.java
@@ -0,0 +1,43 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class UserAddPost {
+
+    private static final String USER_API = "/blog/user/{id}/post";
+
+    @Test
+    public void shouldAllowConfirmedUserAddPost() {
+        JSONObject jsonObj = new JSONObject().put("entry", "Confirmed user post");
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_CREATED)
+                   .when()
+                   .post(USER_API, 1);
+    }
+
+    @Test
+    public void shouldNotAllowUnconfirmedUserAddPost() {
+        JSONObject jsonObj = new JSONObject().put("entry", "Confirmed user post");
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_CREATED)
+                   .when()
+                   .post(USER_API, 2);
+    }
+}
