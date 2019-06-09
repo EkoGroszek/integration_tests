cfa720548eed35ee29318d3a0483144fdb95913f
p.wolski@polsource.com <pa-wo97@o2.pl>
2019-06-09 14:27:37 +0200

    Create tests for error and confirmed for liked post

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -11,9 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import edu.iis.mto.blog.api.request.PostRequest;
 import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.domain.errors.DomainError;
 import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
@@ -24,20 +28,52 @@ public class BlogManagerTest {
 
     @MockBean
     UserRepository userRepository;
-
     @Autowired
     BlogDataMapper dataMapper;
-
     @Autowired
     BlogService blogService;
+    @Autowired
+    public LikePostRepository likePostRespository;
 
     @Test
     public void creatingNewUserShouldSetAccountStatusToNEW() {
         blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
         ArgumentCaptor<User> userParam = ArgumentCaptor.forClass(User.class);
-        Mockito.verify(userRepository).save(userParam.capture());
+        Mockito.verify(userRepository)
+               .save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+    @Test(expected = DomainError.class)
+    public void isThorwErrorIfUserLikedPostDifferentThenConfirmed() {
+        Long thisUserId = blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
+        Long thisPostId = blogService.createPost(thisUserId, new PostRequest());
+
+        blogService.addLikeToPost(thisUserId, thisPostId);
+
+        ArgumentCaptor<LikePost> likedPost = ArgumentCaptor.forClass(LikePost.class);
+        Mockito.verify(likePostRespository, never())
+               .save(likedPost.capture());
+    }
+
+    public void isAllowConfirmedForUserLikedPost() {
+
+        UserRequest userData = new UserRequest("John", "Steward", "john@domain.com");
+        Long thisUserId = blogService.createUser(userData);
+        Long thisPostId = blogService.createPost(thisUserId, new PostRequest());
+
+        User user = dataMapper.mapToEntity(userData);
+        user.setId(thisUserId);
+        user.setAccountStatus(AccountStatus.CONFIRMED);
+
+        blogService.addLikeToPost(thisUserId, thisPostId);
+        ArgumentCaptor<LikePost> likedPost = ArgumentCaptor.forClass(LikePost.class);
+        Mockito.verify(likePostRespository)
+               .save(likedPost.capture());
+        LikePost liked = likedPost.getValue();
+        Assert.assertThat(liked.getUser(), Matchers.equalTo(user));
+
     }
 
 }
