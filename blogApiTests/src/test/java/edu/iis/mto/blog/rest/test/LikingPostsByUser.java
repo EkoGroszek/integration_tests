f30ff09a38f2b65f8c0e23968c7368ac6522c17a
Kamil Żarski <209526@edu.p.lodz.pl>
2019-06-21 23:12:37 +0200

    3.4.3 Tests for liking post, change import.sql

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikingPostsByUser.java
@@ -0,0 +1,106 @@
+package edu.iis.mto.blog.rest.test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+
+public class LikingPostsByUser extends FunctionalTests {
+
+    @Test
+    public void testFotLikingOwnPost() {
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post("/blog/user/1/like/1");
+    }
+
+    @Test
+    public void testForPostingByNotConfirmedUsers() {
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post("/blog/user/2/like/1");
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post("/blog/user/4/like/1");
+    }
+
+    @Test
+    public void testForDoubleLikingSamePost() {
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post("/blog/user/3/like/1");
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post("/blog/user/3/like/1");
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .when()
+                .get("/blog/user/1/post")
+                .then()
+                .body("likesCount", hasItem(1));
+    }
+
+    @Test
+    public void testForLikingBySomeoneOtherUser() {
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post("/blog/user/1/like/2");
+    }
+}
