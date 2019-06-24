230028e98a8edfa1743bdcd4bb6086f167f068fb
Bartłomiej Dettlaff <bartlomiejdettlaff@interia.pl>
2019-06-24 12:46:52 +0200

    First Test in Search User Post

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUserPostsTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUserPostsTest.java
@@ -1,4 +1,26 @@
 package edu.iis.mto.blog.rest.test;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
 public class SearchUserPostsTest extends FunctionalTests {
+
+    private static final String GET_API_START = "/blog/user/";
+    private static final String GET_API_END = "/post";
+
+    @Test
+    public void searchOfPostOfRemovedUserShouldReturnBadRequest() {
+        String userId = "3";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(GET_API_START + userId + GET_API_END);
+    }
 }
