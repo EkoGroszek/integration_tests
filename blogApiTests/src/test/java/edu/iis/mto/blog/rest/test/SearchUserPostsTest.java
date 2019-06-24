cfcf322057f568e83785748e224d64c7ba20392c
Bartłomiej Dettlaff <bartlomiejdettlaff@interia.pl>
2019-06-24 12:48:44 +0200

    Rest of tests add to search user posts test.

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUserPostsTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUserPostsTest.java
@@ -5,6 +5,9 @@ import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.core.Is.is;
+
 public class SearchUserPostsTest extends FunctionalTests {
 
     private static final String GET_API_START = "/blog/user/";
@@ -22,5 +25,82 @@ public class SearchUserPostsTest extends FunctionalTests {
                 .statusCode(HttpStatus.SC_BAD_REQUEST)
                 .when()
                 .post(GET_API_START + userId + GET_API_END);
+    }
+
+    @Test
+    public void searchOfPostOfUserWithNoPostShouldReturnNothing() {
+        String userId = "2";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("size()", is(0))
+                .when()
+                .get(GET_API_START + userId + GET_API_END);
+    }
+
+    @Test
+    public void searchOfPostsOfUserWithMultiplePostShouldReturnProperAmount() {
+        String userId = "4";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("size()", is(2))
+                .when()
+                .get(GET_API_START + userId + GET_API_END);
+    }
+
+    @Test
+    public void searchOfPostOfUserShouldReturnPostWithProperLikesAmountWhenPostHasLikes() {
+        String userId = "1";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post("/blog/user/4/like/4");
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("size()", is(1))
+                .and()
+                .body("likesCount", hasItems(1))
+                .when()
+                .get(GET_API_START + userId + GET_API_END);
+    }
+
+    @Test
+    public void searchOfPostOfUserShouldReturnPostWithProperLikesAmountWhenPostHasNoLikes() {
+        String userId = "5";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("size()", is(1))
+                .and()
+                .body("likesCount", hasItems(0))
+                .when()
+                .get(GET_API_START + userId + GET_API_END);
     }
 }
