d93da2144c7c58f33db9ed93468ee6a3df1b098d
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-07-05 01:28:17 +0200

    3.3 - test for no existing user added

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -1,9 +1,12 @@
 package edu.iis.mto.blog.api;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.dto.Id;
+import edu.iis.mto.blog.services.BlogService;
+import edu.iis.mto.blog.services.DataFinder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -14,13 +17,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import edu.iis.mto.blog.api.request.UserRequest;
-import edu.iis.mto.blog.dto.Id;
-import edu.iis.mto.blog.services.BlogService;
-import edu.iis.mto.blog.services.DataFinder;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(BlogApi.class)
@@ -48,6 +48,12 @@ public class BlogApiTest {
         mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON_UTF8)
                 .accept(MediaType.APPLICATION_JSON_UTF8).content(content)).andExpect(status().isCreated())
                 .andExpect(content().string(writeJson(new Id(newUserId))));
+    }
+
+    @Test
+    public void shouldReturn404IfUserNotFoundWhenTryToGetUserThatNotExist() throws Exception {
+        Mockito.when(finder.getUserData(0L)).thenThrow(new DomainError(DomainError.USER_NOT_FOUND));
+        mvc.perform(get("/blog/user/{id}", 0)).andExpect(status().isNotFound());
     }
 
     private String writeJson(Object obj) throws JsonProcessingException {
