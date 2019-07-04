e332b02f0264bf6fced32998496d3526a83594ea
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-07-05 01:19:19 +0200

    3.2 - test for users with another status than "CONFIRMED" added

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,5 +1,15 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.User;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
+import edu.iis.mto.blog.domain.repository.UserRepository;
+import edu.iis.mto.blog.mapper.BlogDataMapper;
+import edu.iis.mto.blog.services.BlogService;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,16 +21,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import edu.iis.mto.blog.api.request.UserRequest;
-import edu.iis.mto.blog.domain.model.AccountStatus;
-import edu.iis.mto.blog.domain.model.User;
-import edu.iis.mto.blog.domain.repository.UserRepository;
-import edu.iis.mto.blog.mapper.BlogDataMapper;
-import edu.iis.mto.blog.services.BlogService;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
+
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
+    @MockBean
+    LikePostRepository likedPostRepository;
 
     @MockBean
     UserRepository userRepository;
@@ -39,5 +50,16 @@ public class BlogManagerTest {
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
+
+    @Test(expected = DomainError.class )
+    public void shouldThrowExceptionWhenNewUserTryToLikePost() {
+        User user = new User();
+        when(userRepository.findById(1L)).thenReturn(java.util.Optional.of(user));
+        BlogPost blogPost = new BlogPost();
+        blogPost.setUser(user);
+        when(blogPostRepository.findById(1L)).thenReturn(java.util.Optional.of(blogPost));
+        blogService.addLikeToPost(user.getId(), blogPost.getId());
+    }
+
 
 }
